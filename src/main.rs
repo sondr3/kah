1c40dbdfa807aa37a6322594bb52e22ed7296052
Sondre Nilsen <nilsen.sondre@gmail.com>
2018-08-29 20:01:41 +0200

    Add structopt, rustfmt for formatting

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,20 @@
+#[macro_use]
+extern crate structopt;
 
+use std::path::PathBuf;
+use structopt::StructOpt;
+
+#[derive(StructOpt, Debug)]
+#[structopt(name="kattis-rs")]
+struct Opt {
+    #[structopt(short = "l", long = "language", parse(from_os_str))]
+    language: PathBuf,
+
+    #[structopt(short = "f", long = "file", parse(from_os_str))]
+    file: PathBuf,
+}
 
 fn main() {
-    println!("Hello, world!");
+    let opt = Opt::from_args();
+    println!("{:?}", opt);
 }
