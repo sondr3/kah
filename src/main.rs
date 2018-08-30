8c8b6ff90c5bb733ca207c6082d11b0aa32c45dd
Sondre Nilsen <nilsen.sondre@gmail.com>
2018-08-30 17:42:59 +0200

    Get and download Kattis files to temporary directory

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
 use structopt::StructOpt;
+use tempfile::tempdir;
+use std::fs::File;
+use std::io::{Write, Read};
 
 #[derive(StructOpt, Debug)]
 #[structopt(name = "kattis-rs", about = "a simple Kattis helper utility")]
@@ -44,20 +47,35 @@ fn kattis_file_path(id: String) -> String {
     String::from(format!("problems/{}/file/statement/samples.zip", id))
 }
 
-fn get_kattis_sample(url: String, id: String) {
+fn get_kattis_sample(url: String, id: String) -> Result<(), Box<std::error::Error>> {
+    let dir = tempdir()?;
+    let file_path = dir.path().join("samples.zip");
+
+    println!("{:?}", dir);
+    println!("{:?}", file_path);
+
+    let mut file = File::create(file_path)?;
+    let mut buffer = Vec::new();
+
     let path: String = format!("{}/{}", url, &kattis_file_path(id));
-    let res = reqwest::get(&path).expect("Whoops");
+    let mut response = reqwest::get(&path)?;
+    response.read_to_end(&mut buffer)?;
 
     println!("{}", path);
-    println!("Status: {}", res.status());
-    println!("Headers:\n{:?}", res.headers());
+    println!("Status: {}", response.status());
+    println!("Headers:\n{:?}", response.headers());
+
+    file.write(&mut buffer)?;
+    dir.close()?;
+    Ok(())
 }
 
-fn test_kattis() {
+fn test_kattis() -> Result<(), Box<std::error::Error>> {
     println!("YOU ARE TESTING ME");
+    Ok(())
 }
 
-fn main() {
+fn main() -> Result<(), Box<std::error::Error>> {
     let java = LanguageInfo {
         name: Language::Java,
         extension: "java".to_string(),
@@ -71,12 +89,13 @@ fn main() {
             id: _,
             name: _,
             url: _,
-        } => get_kattis_sample("https://open.kattis.com".to_string(), "trik".to_string()),
+        } => get_kattis_sample("https://open.kattis.com".to_string(), "trik".to_string())?,
         Opt::Test {
             file: _,
             language: _,
-        } => test_kattis(),
+        } => test_kattis()?,
     }
 
     println!("{:?}", opt);
+    Ok(())
 }
