f8b675d62a362bfd0c97d2b61ca0c0b00942a1b9
Sondre Nilsen <nilsen.sondre@gmail.com>
2018-08-30 23:53:00 +0200

    Formt files with rustfmt

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
+use std::fs::{create_dir_all, remove_file, File};
+use std::io;
+use std::io::{Read, Write};
+use std::path::PathBuf;
 use structopt::StructOpt;
 use tempfile::tempdir;
-use std::fs::{File, create_dir_all, remove_file};
-use std::io::{Write, Read};
-use std::io;
-use std::path::PathBuf;
 
 #[derive(StructOpt, PartialEq, Debug)]
 #[structopt(name = "kattis-rs", about = "a simple Kattis helper utility")]
@@ -102,7 +102,12 @@ fn unzip(file_name: PathBuf) -> Result<(), Box<std::error::Error>> {
         let mut file = archive.by_index(i)?;
         let outpath = file.sanitized_name();
 
-        println!("File {} extracted to \"{}\" ({} bytes)", i, outpath.as_path().display(), file.size());
+        println!(
+            "File {} extracted to \"{}\" ({} bytes)",
+            i,
+            outpath.as_path().display(),
+            file.size()
+        );
         if let Some(p) = outpath.parent() {
             if !p.exists() {
                 create_dir_all(&p).unwrap();
